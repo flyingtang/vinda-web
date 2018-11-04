a5809d06c5848c2dc8b3364765eae0f66f5a5a3b
xiaogang <715709084@qq.com>
2018-11-04 22:44:20 +0800

    update config

--- .umirc.pro.js
+++ .umirc.pro.js
@@ -1,17 +1,17 @@
 export default {
     "proxy": {
         "/api/v1/video": {
-        "target": "http://vinda-video:3001",
+        "target": "vinda-video:3001",
         "changeOrigin": true,
         // "pathRewrite": { "^/api/v1/video" : "/api/v1/video" }
         },
         "/api/v1": {
-        "target": "http://vinda-article:3000",
+        "target": "vinda-article:3000",
         "changeOrigin": true,
         // "pathRewrite": { "^/api" : "" }
         },
         "/static": {
-        "target": "http://vinda-article:3000",
+        "target": "vinda-article:3000",
         "changeOrigin": true
         },
     },
