5ac09c881b118bee32b6bb250365cb42bd565a4b
txg5214 <715709084@qq.com>
2019-01-06 00:34:39 +0800

    更新

--- .umirc.js
+++ .umirc.js
@@ -6,7 +6,7 @@ export default {
     ['umi-plugin-react', {
       antd: true,
       dva: true,
-      dynamicImport: false,
+      dynamicImport: true,
       title: '我的博客',
       dll: false,
       routes: {
---
+++ dist/5.async.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[5],{"B/Cb":function(e,r,t){"use strict";var n=t("FfR7");Object.defineProperty(r,"__esModule",{value:!0}),r.default=void 0;var d=n(t("rdAL"));n(t("Orjb"));class o extends d.default.Component{constructor(e){super(e)}render(){return d.default.createElement("div",null,this.props.children)}}r.default=o}}]);
